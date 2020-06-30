ae006f163f6e14979895130cf018bdf5300e7eba
Aleksey Chizhov <8mozila8@rambler.ru>
2020-06-30 15:47:29 +0300

    shopping_list

---
+++ src/main/java/ru/geekbrains/persist/ShoppingItem.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.persist;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "shopping_items")
+public class ShoppingItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public  ShoppingItem () {}
+}
